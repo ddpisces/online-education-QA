1d3425e73f5d118adc432b6d01a43e3c8f5d72f3
zz cienet <zzcienet@mbp.local>
2013-10-21 13:42:59 +0800

    new ‘Home’ xib files

---
+++ Demo/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  GHSidebarNav
+//
+//  Created by zz cienet on 10/21/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
